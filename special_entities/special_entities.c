961519c264a07b3b7a6a8c1b018d6e080e52e217
Gary Lin <glin@novell.com>
2010-05-11 10:52:45 +0800

    Make the entity table more flexible
    1. a more flexible entity table
    2. fix a stupid no return bug...

--- special_entities/special_entities.c
+++ special_entities/special_entities.c
@@ -1,20 +1,23 @@
 /*
  * html 4.0 special entities:
  * 	http://htmlhelp.com/reference/html40/entities/special.html
+ * List of XML and HTML character entity references
+ *      http://en.wikipedia.org/wiki/List_of_XML_and_HTML_character_entity_references
  * */
 
 #include <glib.h>
 
-/* TODO Make the table more flexible */
-#define ENTITY_NUM 4
-#define ENTITY_TYPES 3
-
-char *special_entities[ENTITY_NUM][ENTITY_TYPES+1] = {
-{"\"", "&quot;", "&#34;", "&#x22;"},
-{"&",  "&amp;",  "&#38;", "&#x26;"},
-{"<",  "&lt;",   "&#60;", "&#x3C;"},
-{">",  "&gt;",   "&#62;", "&#x3E;"}
+/* Entity types: ASCII ENTITY DECIMAL HEX */
+#define ENTITY_TYPES 4
+char *special_entities[][ENTITY_TYPES] = {
+  {"\"", "&quot;", "&#34;", "&#x22;"},
+  {"&",  "&amp;",  "&#38;", "&#x26;"},
+  {"\'", "&apos;", "&#39;", "&#x27;"},
+  {"<",  "&lt;",   "&#60;", "&#x3C;"},
+  {">",  "&gt;",   "&#62;", "&#x3E;"}
 };
+
+int entity_num = sizeof (special_entities) / (sizeof (char *) * ENTITY_TYPES);
 
 char *
 translate_entities(char *in_text)
@@ -25,8 +28,8 @@ translate_entities(char *in_text)
 
 	input = g_strdup (in_text);
 
-	for (i = 0; i < ENTITY_NUM; i++){
-		for (j = 1; j < ENTITY_TYPES+1; j++){
+	for (i = 0; i < entity_num; i++){
+		for (j = 1; j < ENTITY_TYPES; j++){
 			split_text = g_strsplit (input,
 						 special_entities[i][j],
 						 -1);
@@ -37,6 +40,8 @@ translate_entities(char *in_text)
 			input = output;
 		}
 	}
+
+	return output;
 }
 
 int
@@ -44,10 +49,12 @@ main()
 {
 	char *test = ">, &gt;, &#62;, &#x3E;, "
                      "\", &quot;, &#34;, &#x22;, "
+                     "\', &apos;, &#39;, &#x27;, "
                      "&, &amp;, &#38;, &#x26;, "
                      "<, &lt;, &#60;, &#x3C;";
 
 	g_print ("%s\n", translate_entities (test));
+	g_print ("%d\n", entity_num);
 
 	return 0;
 }
