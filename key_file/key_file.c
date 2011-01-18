e40c39110baee67845faa89772aa9ea30dd8df36
Gary Ching-Pang Lin <chingpang@gmail.com>
2011-01-18 11:02:30 +0800

    key file parsing

---
+++ key_file/key_file.c
@@ -0,0 +1,27 @@
+#include <glib.h>
+
+int
+main ()
+{
+	GKeyFile *key_file;
+	gsize length;
+	char **key_list;
+	int i;
+
+	key_file = g_key_file_new ();
+
+	if (!g_key_file_load_from_file (key_file, "key.conf", G_KEY_FILE_NONE, NULL)) {
+		g_key_file_free (key_file);
+		return 1;
+	}
+
+	key_list = g_key_file_get_keys (key_file, "devices", &length, NULL);
+
+	for (i = 0; i < length; i++) {
+		g_printf ("%s\n", key_list[i]);
+	}
+
+	g_key_file_free (key_file);
+
+	return 0;
+}
