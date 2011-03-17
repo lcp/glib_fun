1c978fa65a7587005f99bc211b18a461802e479e
Gary Ching-Pang Lin <chingpang@gmail.com>
2011-03-17 14:59:37 +0800

    key_file: key file string test

--- key_file/key_file.c
+++ key_file/key_file.c
@@ -6,6 +6,8 @@ main ()
 	GKeyFile *key_file;
 	gsize length;
 	char **key_list;
+	char *tmp;
+	char **str_list;
 	int i;
 	gboolean enable_input;
 	gboolean ret;
@@ -27,7 +29,9 @@ main ()
 	}
 
 	for (i = 0; key_list[i] != NULL; i++) {
-		g_printf ("%s\n", key_list[i]);
+		tmp = g_key_file_get_string (key_file, "input devices", key_list[i], NULL);
+		g_printf ("%s = %s\n", key_list[i], tmp);
+		g_free (tmp);
 	}
 
 	enable_input = g_key_file_get_boolean (key_file, "general", "enable input", NULL);
@@ -47,8 +51,14 @@ main ()
 	ret = g_key_file_get_boolean (key_file, "general", "test2", &error);
 	g_printf ("test2 ret = %d\n", ret);
 
+	str_list = g_key_file_get_string_list (key_file, "string list", "list", &length, NULL);
+	for (i = 0; str_list[i] != NULL; i++) {
+		g_printf ("string%d = %s\n", i, str_list[i]);
+	}
+
 	g_key_file_free (key_file);
 	g_strfreev (key_list);
+	g_strfreev (str_list);
 
 	return 0;
 }
