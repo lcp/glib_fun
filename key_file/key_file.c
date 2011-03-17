99fc02fff81b39f3cddec15251a03ec17f57f51d
Gary Ching-Pang Lin <chingpang@gmail.com>
2011-03-17 14:01:12 +0800

    key_file: some more tests

--- key_file/key_file.c
+++ key_file/key_file.c
@@ -7,6 +7,9 @@ main ()
 	gsize length;
 	char **key_list;
 	int i;
+	gboolean enable_input;
+	gboolean ret;
+	GError *error = NULL;
 
 	key_file = g_key_file_new ();
 
@@ -15,7 +18,7 @@ main ()
 		return 1;
 	}
 
-	key_list = g_key_file_get_keys (key_file, "devices", &length, NULL);
+	key_list = g_key_file_get_keys (key_file, "input devices", &length, NULL);
 
 	if (key_list == NULL) {
 		g_printf ("key list is NULL\n");
@@ -26,6 +29,23 @@ main ()
 	for (i = 0; key_list[i] != NULL; i++) {
 		g_printf ("%s\n", key_list[i]);
 	}
+
+	enable_input = g_key_file_get_boolean (key_file, "general", "enable input", NULL);
+
+	if (enable_input)
+		g_printf ("enable input\n");
+	else
+		g_printf ("disable input\n");
+
+	error = NULL;
+	ret = g_key_file_get_boolean (key_file, "general", "test1", &error);
+	g_printf ("test1 ret = %d\n", ret);
+	if (error)
+		g_printf ("error: %s\n", error->message);
+
+	error = NULL;
+	ret = g_key_file_get_boolean (key_file, "general", "test2", &error);
+	g_printf ("test2 ret = %d\n", ret);
 
 	g_key_file_free (key_file);
 	g_strfreev (key_list);
